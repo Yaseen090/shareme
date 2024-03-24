0cbcf1bf204af2298d83b7e7a2c33f48af1956a9
Yaseen Ali <82824015+Yaseen090@users.noreply.github.com>
2024-03-24 05:30:49 +0500

    upto pins

---
+++ shareme_backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'd9l6lny3',
+    dataset: 'production'
+  }
+})
---
+++ shareme_backend/schemaTypes/index.js
@@ -0,0 +1,7 @@
+import user from './user'
+import pin from './pin'
+import save from './save'
+import postedBy from './postedBy'
+import comments from './comment'
+
+export const schemaTypes = [user,pin,comments,postedBy,save]
